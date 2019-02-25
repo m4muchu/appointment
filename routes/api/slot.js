d2cf8cb40d28ef55543471c6aab0e58b8270d33f
m4muchu <muchu.pkd123@gmail.com>
2019-02-25 11:48:21 +0530

    login sytem taken off

--- routes/api/slot.js
+++ routes/api/slot.js
@@ -1,29 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const mogoose = require('mongoose');
-const passport = require('passport');
 
 // import model
 
 const Slots = require('../../models/Slots');
 
-router.post(
-  '/book',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    const newSlot = new Slots({
-      slot_time: req.body.slot_time,
-      slot_date: req.body.slot_date,
-      name: req.user.name,
-      contact: req.user.contact,
-      user: req.user.id
-    });
+router.post('/book', (req, res) => {
+  const newSlot = new Slots({
+    slot_time: req.body.slot_time,
+    slot_date: req.body.slot_date,
+    name: req.user.name,
+    contact: req.user.contact,
+    user: req.user.id
+  });
 
-    newSlot.save().then(slot => {
-      res.json(slot);
-    });
-  }
-);
+  newSlot.save().then(slot => {
+    res.json(slot);
+  });
+});
 
 router.get('/booked', (req, res) => {
   Slots.find()
@@ -50,24 +44,20 @@ router.get('/booked', (req, res) => {
 //@desc delete slot by id
 //@access private
 
-router.delete(
-  '/:id',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Slots.findById(req.params.id)
-      .then(slot => {
-        // check slot owner, slot created owner can only do the delete option
-        if (slot.user.toString() !== req.user.id) {
-          return res.status(401).json({ unAuthorised: 'user not authorised' });
-        }
-        // delete the slot
-        slot.remove().then(() => {
-          res.json({ success: true });
-        });
-      })
-      .catch(err => res.status(404).json({ slotNotFound: 'Slot not found' }));
-  }
-);
+router.delete('/:id', (req, res) => {
+  Slots.findById(req.params.id)
+    .then(slot => {
+      // check slot owner, slot created owner can only do the delete option
+      if (slot.user.toString() !== req.user.id) {
+        return res.status(401).json({ unAuthorised: 'user not authorised' });
+      }
+      // delete the slot
+      slot.remove().then(() => {
+        res.json({ success: true });
+      });
+    })
+    .catch(err => res.status(404).json({ slotNotFound: 'Slot not found' }));
+});
 
 // router.post(
 //   '/fetch',
@@ -79,48 +69,40 @@ router.delete(
 //   }
 // );
 
-router.post(
-  '/bookedDates',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    console.log(req.body.slot_date);
-    Slots.find({ slot_date: req.body.slot_date })
-      .then(slots => {
-        res.json(slots);
-      })
-      .catch(err =>
-        res.status(404).json({ slotNotFound: 'No slot available at this date' })
-      );
-  }
-);
+router.post('/bookedDates', (req, res) => {
+  console.log(req.body.slot_date);
+  Slots.find({ slot_date: req.body.slot_date })
+    .then(slots => {
+      res.json(slots);
+    })
+    .catch(err =>
+      res.status(404).json({ slotNotFound: 'No slot available at this date' })
+    );
+});
 
-router.post(
-  '/bookedSlotTime',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Slots.find({ slot_date: req.body.slot_date }).then(slots => {
-      const defaultTime = [
-        '1AM-2AM',
-        '2AM-3AM',
-        '3AM-4AM',
-        '4AM-5AM',
-        '5AM-6AM',
-        '6AM-7AM',
-        '7AM-8AM',
-        '8AM-9AM'
-      ];
-      let reminedTime = [];
-      let bookedTime = [];
+router.post('/bookedSlotTime', (req, res) => {
+  Slots.find({ slot_date: req.body.slot_date }).then(slots => {
+    const defaultTime = [
+      '1AM-2AM',
+      '2AM-3AM',
+      '3AM-4AM',
+      '4AM-5AM',
+      '5AM-6AM',
+      '6AM-7AM',
+      '7AM-8AM',
+      '8AM-9AM'
+    ];
+    let reminedTime = [];
+    let bookedTime = [];
 
-      for (let key in slots) {
-        bookedTime[key] = slots[key].slot_time;
-      }
+    for (let key in slots) {
+      bookedTime[key] = slots[key].slot_time;
+    }
 
-      reminedTime = defaultTime.filter(val => !bookedTime.includes(val));
+    reminedTime = defaultTime.filter(val => !bookedTime.includes(val));
 
-      res.json(reminedTime);
-    });
-  }
-);
+    res.json(reminedTime);
+  });
+});
 
 module.exports = router;
